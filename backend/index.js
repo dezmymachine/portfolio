ed63ff2a63efa16acf5f4b623ef9950f8e98bdf9
dezmymachine <desmondkwakucoffie195@gmail.com>
2024-03-20 13:56:59 +0000

    created routes for blogs

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,8 @@ import cors from "cors";
 import skills from "./routes/skills.routes.js";
 import experience from "./routes/experiences.routes.js";
 import project from "./routes/projects.routes.js";
+import achievement from "./routes/achievements.routes.js";
+import blogs from "./routes/blogs.routes.js";
 
 const PORT = process.env.PORT || 4000;
 
@@ -17,6 +19,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use("/skills", skills);
 app.use("/experience", experience);
 app.use("/projects", project);
+app.use("/achievement", achievement);
+app.use("/blogs", blogs);
 //start the server
 app.listen(PORT, () => {
   console.log(`Server is running on port:${PORT}`);
--- backend/routes/achievements.routes.js
+++ backend/routes/achievements.routes.js
@@ -1,1 +1,65 @@
- 
+import { Router } from "express";
+import db from "../db/connection.js";
+import { ObjectId } from "mongodb";
+
+const router = Router();
+const ACHIEVEMENT_COLLECTION = db.collection("achievement");
+
+//Endpoint for getting list of achievements
+
+router.get("/", async (req, res) => {
+  let results = await ACHIEVEMENT_COLLECTION.find({}).toArray();
+  res.send(results).status(200);
+});
+
+//Endpoint for getting a single achievement by id
+router.get("/:id", async (req, res) => {
+  let query = { _id: new ObjectId(req.params.id) };
+  let result = await ACHIEVEMENT_COLLECTION.findOne(query);
+
+  !result ? res.send("Not found").status(404) : res.send(result).status(200);
+});
+
+//Endpoint for adding a single achievement
+router.post("/", async (req, res) => {
+  try {
+    let newAchievement = {
+      achievement: req.body.achievement,
+      description: req.body.description,
+    };
+    let result = await achievementS_COLLECTION.insertOne(newAchievement);
+    res.send(result).status(201);
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+//Endpoint for updating a achievement by id
+router.patch("/:id", async (req, res) => {
+  try {
+    const query = { _id: new ObjectId(req.params.id) };
+    const updates = {
+      $set: {
+        achievement: req.body.achievement,
+        description: req.body.description,
+      },
+    };
+
+    let result = await ACHIEVEMENT_COLLECTION.updateOne(query, updates);
+    res.send(result).status(201);
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+//Endpoint for deleting a achievement by id
+router.delete("/:id", async (req, res) => {
+  try {
+    const query = { _id: new ObjectId(req.params.id) };
+    let result = await ACHIEVEMENT_COLLECTION.deleteOne(query);
+    res.send(result).status(200);
+  } catch (error) {
+    console.error(error);
+  }
+});
+export default router;
